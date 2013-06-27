67ec64587bf0a23dfac7427716c47c58f296a210
Bart Vollebregt <bartvollebregt@hotmail.com>
2013-06-27 16:59:27 +0200

    Spotlights weergeven met de API

---
+++ get_spotlights.php
@@ -0,0 +1,54 @@
+<?php header('content-type: application/json; charset=utf-8');
+
+/*
+ * Following code will list all the places
+ */
+
+// array for JSON response
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connecting to db
+$db = new DB_CONNECT();
+
+// get all places from places table
+$result = mysql_query("SELECT * FROM monumenten WHERE `uitgelicht` = 1 ORDER BY title ASC") or die(mysql_error());
+
+// check for empty result
+if (mysql_num_rows($result) > 0) {
+    // looping through all results
+    // places node
+    $response["places"] = array();
+    
+    while ($row = mysql_fetch_array($result)) {
+        // temp user array
+        $places = array();
+        $places["ID"] = $row["ID"];
+        $places["title"] = $row["title"];
+        $places["address"] = $row["address"];
+        $places["city"] = $row["city"];
+        $places["latitude"] = $row["latitude"];
+		$places["longitude"] = $row["longitude"];
+		$places["category"] = $row["category"];
+		$places["uitgelicht"] = $row["uitgelicht"];
+
+
+        // push single places into final response array
+        array_push($response["places"], $places);
+    }
+    // success
+    $response["success"] = 1;
+
+    // echoing JSON response
+    echo json_encode($response);
+} else {
+    // no places found
+    $response["success"] = 0;
+    $response["message"] = "No places found";
+
+    // echo no users JSON
+    echo json_encode($response);
+}
+?>
